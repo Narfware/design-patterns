021ba91a2fd45292124bb9f9916ce789b8d9a5b5
Narfware <narfware@gmail.com>
2023-12-22 20:18:34 +0100

    add test to the generic factory

---
+++ src/factory/index.test.ts
@@ -0,0 +1,17 @@
+import { CarDealerShip, HyperCar, TourismCar } from '.'
+
+test('should be an instance of TourismCar', () => {
+  const factory = new CarDealerShip()
+
+  const car = factory.createCar(250)
+
+  expect(car).toBeInstanceOf(TourismCar)
+})
+
+test('should be an instance of HyperCar', () => {
+  const factory = new CarDealerShip()
+
+  const car = factory.createCar(500)
+
+  expect(car).toBeInstanceOf(HyperCar)
+})
