154a4169aa2514c6ca3fe1f0db528fdac6d90a68
Narfware <narfware@gmail.com>
2023-12-28 23:49:24 +0100

    add async event bus

---
+++ src/observer/in-memory-async-event-bus.ts
@@ -0,0 +1,20 @@
+import { EventEmitter } from 'events'
+import { type EventBus, type Event, type EventSubscriber } from './event-bus'
+
+export class InMemoryAsyncEventBus extends EventEmitter implements EventBus {
+  async publish (events: Event[]): Promise<void> {
+    events.forEach(event => this.emit(event.eventName, event))
+  }
+
+  public addSubscribers (subscribers: Set<EventSubscriber<Event>>): void {
+    subscribers.forEach(subscriber => {
+      this.attachSubscriberToEvent(subscriber.subscribedTo(), subscriber)
+    })
+  }
+
+  private attachSubscriberToEvent (subscribedTo: Set<string>, subscriber: EventSubscriber<Event>): void {
+    subscribedTo.forEach(event => {
+      this.on(event, subscriber.on.bind(subscriber))
+    })
+  }
+}
