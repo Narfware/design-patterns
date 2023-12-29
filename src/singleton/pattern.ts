cf4e2bef738294af9b0eaf08d478f0df692f68c6
Narfware <narfware@gmail.com>
2023-12-29 14:15:11 +0100

    remove unnecessary property and add eslint rule

--- src/singleton/pattern.ts
+++ src/singleton/pattern.ts
@@ -1,20 +1,10 @@
 export class Singleton {
   private static instance: Singleton
-  private counter: number
-
-  private constructor () {
-    this.counter = 0
-  }
-
   public static getInstance (): Singleton {
     if (Singleton.instance != null) return Singleton.instance
 
     Singleton.instance = new Singleton()
 
     return Singleton.instance
-  }
-
-  public incrementCounter (): void {
-    this.counter += this.counter
   }
 }
