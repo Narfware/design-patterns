d78bdc1a74140078e49b196140691b455773c972
Narfware <narfware@gmail.com>
2023-12-29 14:07:49 +0100

    fix bad property

--- src/observer/in-memory-async-event-bus.ts
+++ src/observer/in-memory-async-event-bus.ts
@@ -3,7 +3,7 @@ import { type EventBus, type Event, type EventSubscriber } from './event-bus'
 
 export class InMemoryAsyncEventBus extends EventEmitter implements EventBus {
   async publish (events: Event[]): Promise<void> {
-    events.forEach(event => this.emit(event.eventName, event))
+    events.forEach(event => this.emit(event.eventName(), event))
   }
 
   public addSubscribers (subscribers: Set<EventSubscriber<Event>>): void {
